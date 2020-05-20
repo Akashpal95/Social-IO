4b7e240b7b7bd6704abe477f8f6e80dddcbd61c9
Akashpal95 <akash.pal95@gmail.com>
2020-05-21 04:38:29 +0530

    First Commit:Create the directory structure and setup express structure

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000; //port 80 for production level code
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server : ${err}`);
+    }
+    console.log(`Server is running on port : ${port}`);
+});
