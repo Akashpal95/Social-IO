652d16e243b127f0ecb789570be298a4e275c9d2
Akashpal95 <akash.pal95@gmail.com>
2020-05-21 12:44:15 +0530

    Set up view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000; //port 80 for production level code
 
 app.use('/', require('./routes'))
 
+//Setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
